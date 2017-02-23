576268da00b34233bf6ce58357ac3c444dbaf1f6
pedkafkaf <pediie12@hotmail.com>
2017-02-23 16:28:42 +0700

    show listview

--- app/src/main/java/comsci/ped/myapplication1/MainActivity.java
+++ app/src/main/java/comsci/ped/myapplication1/MainActivity.java
@@ -5,21 +5,42 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
+    ListView listView;
+    private int[] ints = new int[]{R.drawable.traffic_01,R.drawable.traffic_02,
+            R.drawable.traffic_03,R.drawable.traffic_04,R.drawable.traffic_05,
+            R.drawable.traffic_06,R.drawable.traffic_07,R.drawable.traffic_08,
+            R.drawable.traffic_09,R.drawable.traffic_10,R.drawable.traffic_11,
+            R.drawable.traffic_12,R.drawable.traffic_13,R.drawable.traffic_14,
+            R.drawable.traffic_15,R.drawable.traffic_16,R.drawable.traffic_17,
+            R.drawable.traffic_18,R.drawable.traffic_19,R.drawable.traffic_20};
+
+    private String[] titelstrings,deitailStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }  //main method onCreate
+
+        //Intital View ผูกตัวแปรกับไอดีของ view
+        listView = (ListView) findViewById(R.id.livTraffic);
+
+        // get value .xml File by My_content.xml to display
+        titelstrings = getResources().getStringArray(R.array.title);
+        deitailStrings = getResources().getStringArray(R.array.detail);
+
+        //create Listview
+        MyAdapter myAdapter = new MyAdapter(MainActivity.this,ints,titelstrings,deitailStrings);
+        listView.setAdapter(myAdapter);
+    }// main methot onCreate
 
     public void onClickMoreInfo(View view){
-
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse("https://www.dlt.go.th/th/dlt-knowledge/view.php?_did=111"));
         startActivity(intent);
+    }// main methot onClickMoreInfo
 
-    } //main mathod onClickMoreInfo
-
-}  //main Class
+}// main class
